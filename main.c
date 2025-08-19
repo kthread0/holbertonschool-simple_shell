9cad1d21b59fb0dab3af3bf80f1d78cc3b637c47
Lucas Noble <166522493+Jackpot-007@users.noreply.github.com>
2025-08-19 10:51:11 -0300

    agregue cosas en el main.c

--- main.c
+++ main.c
@@ -9,30 +9,30 @@
  */
 int main(void)
 {
-	char *lineptr = NULL;
-	size_t n = 0;
-	ssize_t input;
+	char *lineptr = NULL; /* Puntero para almacenar la linea leida del usuario */
+	size_t n = 0; /* Variable para el tamaÃo del buffer de lineptr */
+	ssize_t input; /* Variable para almacenar el numero de caracter leido */
 
-	while (1)
+	while (1) /* Bucle infinito para mantener el shell activo */
 	{
 		printf("Stranger Strings> "); /* Prompt */
-		input = getline(&lineptr, &n, stdin);
+		input = getline(&lineptr, &n, stdin); /* Lee la linea de entrada del usuario */
 
-		if (input == -1)
-			break;
+		if (input == -1) /* Verifica si hubo error o fin de archivo (EOF) al leer */
+			break; /* EOF o error */
 
-		if (lineptr[input - 1] == '\n')
-			lineptr[input - 1] = '\0';
+		if (lineptr[input - 1] == '\n') /* Eliminar salto de linea al final */
+			lineptr[input - 1] = '\0'; /* Reemplaza el salto de linea al final por un nulo */
 
-		if (strcmp(lineptr, "exit") == 0)
-			break;
+		if (strcmp(lineptr, "exit") == 0) /* Compara si la entrada es "exit" */
+			break; /* Sale del bucle si el usuario escribio "exit" */
 
-		if (lineptr[0] == '\0')
-			continue;
+		if (lineptr[0] == '\0') /* Comprueba si la linea esta vacia (primer caracter nulo) */
+			continue; /* Salta a la siguiente iteracion del bucle */
 
-		prompt(lineptr);
+		prompt(lineptr); /* Llamamos a prompt con la linea que el usuario escribe */
 	}
 
-	free(lineptr);
+	free(lineptr); /* Libera la memoria asignada a 'lineptr' */
 	return (0);
 }
