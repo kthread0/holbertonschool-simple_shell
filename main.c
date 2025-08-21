9d3106d6a328d6b11f905702540d9087ac2f52e7
EzequielKH <laudarosa12@gmail.com>
2025-08-21 12:42:18 +0000

    betty

--- main.c
+++ main.c
@@ -10,33 +10,22 @@
 int main(void)
 {
 	char *lineptr = NULL;
-	/* Puntero para almacenar la linea leida del usuario */
 	size_t n = 0;
-	/* Variable para el tamaÃƒo del buffer de lineptr */
 	ssize_t input;
-	/* Variable para almacenar el numero de caracter leido */
 	int last_status = 0;
 
 
 	while (1)
-		/* Bucle infinito para mantener el shell activo */
 	{
 		if (isatty(STDIN_FILENO))
-			/* Verifica si la entrada estaÃndar es un terminal*/
 			fprintf(stderr, "Stranger Strings> "); 
-		/* Prompt en stderr*/
 		input = getline(&lineptr, &n, stdin);
-		/* Lee la linea de entrada del usuario */
 
 		if (input == -1)
-			/* Verifica si hubo error o fin de archivo (EOF) al leer */
 			break;
-		/* EOF o error */
 
 		if (lineptr[input - 1] == '\n')
-			/* Eliminar salto de linea al final */
 			lineptr[input - 1] = '\0';
-		/* Reemplaza el salto de linea al final por un nulo */
 
 		if (strcmp(lineptr, "exit") == 0)
 		{
@@ -45,15 +34,11 @@ int main(void)
 
 		}
 		if (lineptr[0] == '\0')
-			/* Comprueba si la linea esta vacia (primer caracter nulo) */
 			continue;
-		/* Salta a la siguiente iteracion del bucle */
 
 		last_status = prompt(lineptr);
-		/* Llamamos a prompt con la linea que el usuario escribe */
 	}
 
 	free(lineptr);
-	/* Libera la memoria asignada a 'lineptr' */
 	return (0);
 }
--- prompt.c
+++ prompt.c
@@ -11,55 +11,43 @@
 int prompt(char *line)
 {
 	char *argv[10];
-	/* Arreglo para almacenar los argumentos del comando */
 	int i = 0;
-	/* Contador para iterar sobre los argumentos */
 	char *token;
-	/* Puntero para almacenar cada segmento de la linea */
 	int status;
 	pid_t child_pid;
 
 	token = strtok(line, " \n");
-	/* Divide la linea en tokens separados por espacio */
 	while (token != NULL && i < 9)
-		/* Itera mientras haya tokens y espacio en el arreglo */
 	{
 		argv[i++] = token;
-		/* Guarda cada token en argv */
 		token = strtok(NULL, " \n");
-		/* Obtiene el siguiente token en la linea */
 	}
 	argv[i] = NULL;
-	/* Termina el arreglo con NULL como pide execvp */
 
 	if (argv[0] == NULL)
-		/* Si la línea esta vac�a, no hace nada */
 		return (0);
 
-	child_pid = fork(); /* Guarda el PID del hijo */
+	child_pid = fork();
 
-	if (child_pid == -1) /* Error al crear el proceso hijo */
+	if (child_pid == -1)
 	{
 		perror("fork failed");
 		return (1);
 	}
 
-	if (child_pid == 0) /* Proceso hijo */
+	if (child_pid == 0)
 	{
-		execvp(argv[0], argv); /* Ejecuta el comando con sus argumentos */
-		/* Si execvp falla */
+		execvp(argv[0], argv);
 		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-		exit(127); /* Código de fallo si no se encontró el coman */
+		exit(127);
 	}
 
-	else /* Se ejecuta en el proceso padre */
+	else
 	{
 		waitpid(child_pid, &status, 0);
-		/* El padre espera al hijo y guarda el estado */
 
 		if (WIFEXITED(status))
 			return WEXITSTATUS(status);
-		/* Devuelve el código de salida real del hijo */
 	}
 
 	return (0);
