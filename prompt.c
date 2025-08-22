75555a0dd418fcae27264d545af404314f4c9702
Jackpot-007 <jackpotmerlon@gmail.com>
2025-08-22 03:35:06 +0000

    arrgle una cosa en el prompt.c

--- prompt.c
+++ prompt.c
@@ -1,21 +1,16 @@
 #include "shell.h"
 
 /**
- * prompt - procesa la linea de entrada,
- * crea un proceso hijo y ejecuta el comando ingresado.
+ * token_input - separa la linea en tokens y llena argv
+ * @line: linea de entrada
+ * @argv: arreglo de punteros a tokens
  *
- * @line: linea de entrada del usuario (cadena de caracteres).
- *
- * Return: 0 en caso de exito.
+ * Return: numero de tokens encontrados
  */
-
-int prompt(char *line)
+int token_input(char *line, char **argv)
 {
-	char *argv[10];
-	int i = 0;
 	char *token;
-	int status;
-	pid_t child_pid;
+	int i = 0;
 
 	token = strtok(line, " \n");
 	while (token != NULL && i < 9)
@@ -25,11 +20,27 @@ int prompt(char *line)
 	}
 	argv[i] = NULL;
 
-	if (argv[0] == NULL)
+	return (i);
+}
+
+/**
+ * prompt - procesa la linea de entrada,
+ * crea un proceso hijo y ejecuta el comando ingresado.
+ * @line: linea de entrada del usuario (cadena de caracteres).
+ *
+ * Return: 0 en caso de exito.
+ */
+int prompt(char *line)
+{
+	char *argv[10];
+	int argc, status;
+	pid_t child_pid;
+
+	argc = token_input(line, argv);
+	if (argc == 0)
 		return (0);
 
 	child_pid = fork();
-
 	if (child_pid == -1)
 	{
 		perror("fork failed");
@@ -42,11 +53,9 @@ int prompt(char *line)
 		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
 		exit(127);
 	}
-
 	else
 	{
 		waitpid(child_pid, &status, 0);
-
 		if (WIFEXITED(status))
 			return (WEXITSTATUS(status));
 	}
