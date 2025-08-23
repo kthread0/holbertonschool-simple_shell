f889d159f2ca76c49a8a36ef11b457a9be4f66ef
Jackpot-007 <jackpotmerlon@gmail.com>
2025-08-23 21:35:59 +0000

    arregle algo en prompt.c

--- prompt.c
+++ prompt.c
@@ -1,6 +1,30 @@
 #include "shell.h"
 
 /**
+ * exec_command - ejecuta un comando usando execvp y
+ * maneja PATH vacio.
+ * @argv: array de strings que representa los argumentos
+ * del comando.
+ *
+ * Si PATH esta vacio y el comando no tiene '/', imprime
+ * error y sale.
+ */
+static void exec_command(char **argv)
+{
+	char *path = getenv("PATH");
+
+	if ((!path || path[0] == '\0') && strchr(argv[0], '/') == NULL)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+		exit(127);
+	}
+
+	execvp(argv[0], argv);
+	fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+	exit(127);
+}
+
+/**
  * prompt - procesa la linea de entrada,
  * crea un proceso hijo y ejecuta el comando ingresado.
  * @line: linea de entrada del usuario.
@@ -9,10 +33,8 @@
  */
 int prompt(char *line)
 {
-	char *argv[10];
-	int i = 0;
-	char *token;
-	int status;
+	char *argv[10], *token;
+	int i = 0, status;
 	pid_t child_pid;
 
 	token = strtok(line, " \n");
@@ -22,38 +44,18 @@ int prompt(char *line)
 		token = strtok(NULL, " \n");
 	}
 	argv[i] = NULL;
-
 	if (argv[0] == NULL)
 		return (0);
 
 	child_pid = fork();
-
 	if (child_pid == -1)
 	{
 		perror("fork failed");
 		return (1);
 	}
-
 	if (child_pid == 0)
-	{
-		char *path = getenv("PATH");
+		exec_command(argv);
 
-		if ((!path || path[0] == '\0') && strchr(argv[0], '/') == NULL)
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-			exit(127);
-		}
-
-		execvp(argv[0], argv);
-		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-		exit(127);
-	}
-	else
-	{
-		waitpid(child_pid, &status, 0);
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-	}
-
-	return (0);
+	waitpid(child_pid, &status, 0);
+	return (WIFEXITED(status) ? WEXITSTATUS(status) : 0);
 }
